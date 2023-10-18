536d2b8dcf9ae37d8f629bbc46faeb1885a02208
Fozzy12 <144604966+Fozzy12@users.noreply.github.com>
2023-10-18 18:06:23 +0500

    Lab6_2

---
+++ Lab6_2/Lab_6/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_6
+{
+    public partial class Lab7 : Form
+    {
+        public Lab7()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int counter = 0;//количество слагаемых
+            double sum = 0;//сумма
+            double summand = 0.0;//слагаемое
+            double x;
+            double precision;
+            bool f = false;
+                        if (double.TryParse(txtPrecision.Text, out precision)) {
+                f = true;
+            }
+            //вычисление суммы ряда
+            do
+            {
+                counter++;
+                if (double.TryParse(txtX.Text, out x) )
+                {
+                    summand = double.Parse(txtX.Text) / counter;
+                }
+                sum += summand;
+            }while (f && Math.Abs(summand) > precision);
+            //результат
+            if (f)
+            {
+                lblResult.Text = "сумма = " + sum + ", количество = " + counter;
+            }
+            //если пользователь ошибся
+            else
+            {
+                MessageBox.Show("введите чиловое значение!!!", "ОШИБКА!");
+            }
+        }
+    }
+}
