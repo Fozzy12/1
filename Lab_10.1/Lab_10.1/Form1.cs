8c1712840401aef7fdbdf8ec2785827687d49241
Fozzy12 <144604966+Fozzy12@users.noreply.github.com>
2023-11-23 23:15:47 +0500

    Lab_10

---
+++ Lab_10.1/Lab_10.1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_10._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonСalc_Click(object sender, EventArgs e)
+        {   
+            // Получаем значения из текстовых полей
+            double Xmin = double.Parse(textBoxXmin.Text);
+            double Xmax = double.Parse(textBoxXmax.Text);
+            double Step = double.Parse(textBoxStep.Text);
+            // Вычисляем количество точек
+            int count = (int)Math.Ceiling((Xmax - Xmin) / Step) + 1;
+            // Создаем массивы для значений x, y1 и y2
+            double[] x = new double[count];
+            double[] y1 = new double[count];
+            double[] y2 = new double[count];
+            // Заполняем массивы значениями
+            for (int i = 0; i < count; i++)
+            {
+                x[i] = Xmin + Step * i;
+                // Задаем значение y1[i] как синус x[i]
+                y1[i] = Math.Sin(x[i]);
+                // Задаем значение y2[i] как косинус x[i]
+                y2[i] = Math.Cos(x[i]);
+            }
+            // Устанавливаем минимальное и максимальное значения оси X на графике
+            chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+            // Устанавливаем интервал сетки по оси X на графике
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step;
+            // Связываем значения массивов с сериями графика
+            chart1.Series[0].Points.DataBindXY(x, y1);
+            chart1.Series[1].Points.DataBindXY(x, y2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form2 button1 = new Form2();
+            button1.ShowDialog();
+            Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Lab_10.1/Lab_10.1/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_10._1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonСalc_Click(object sender, EventArgs e)
+        {
+            // Получаем значения из текстовых полей
+            double Xmin = double.Parse(textBoxXmin.Text);
+            double Xmax = double.Parse(textBoxXmax.Text);
+            double Step = double.Parse(textBoxStep.Text);
+            double b = double.Parse(textB.Text);
+            // Вычисляем количество точек
+            int count = (int)Math.Ceiling((Xmax - Xmin) / Step) + 1;
+            // Создаем массивы для значений x, y1 и y2
+            double[] x = new double[count];
+            double[] y1 = new double[count];
+            double[] y2 = new double[count];
+            // Заполняем массивы значениями
+            for (int i = 0; i < count; i++)
+            {
+                x[i] = Xmin + Step * i;
+                y1[i] = Math.Pow(x[i], 2) + Math.Tan(5 * x[i] + b / x[i]);
+                y2[i] = Math.Sin(x[i]);
+            }
+            // Устанавливаем минимальное и максимальное значения оси X на графике
+            chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+            // Устанавливаем интервал сетки по оси X на графике
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step; 
+            // Связываем значения массивов с сериями графика
+            chart1.Series[0].Points.DataBindXY(x, y1);
+            chart1.Series[1].Points.DataBindXY(x, y2);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
