5ca7a171c3ad8da3cb77f55c90e833d294923889
Fozzy12 <144604966+Fozzy12@users.noreply.github.com>
2023-12-13 22:02:52 +0500

    Lab_11.1

---
+++ Lab_11.1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Lab_11._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(155, 276);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 311);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Lab_11.1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_11._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Создаем объект Graphics для рисования
+            Graphics g = e.Graphics;
+            // Создаем объект pen, который используется для рисования линий, задаем цвет черный
+            Pen pen = new Pen(Color.Black);
+
+            // Создаем объект brush, который используется для заполнения прямоугольника, задаем цвет коричневый
+            Brush brush = new SolidBrush(Color.Brown);
+            // Заполняем прямоугольник
+            g.FillRectangle(brush, 100, 100, 200, 150);
+
+            // Создаем объект brushs, который используется для заполнения треугольника, задаем цвет зеленый
+            Brush brushs = new SolidBrush(Color.Green);
+            // Определяем точки треугольника и заполняем его
+            Point roofPoint1 = new Point(100, 100);
+            Point roofPoint2 = new Point(200, 50);
+            Point roofPoint3 = new Point(300, 100);
+            Point[] roofPoints = { roofPoint1, roofPoint2, roofPoint3 };
+            g.FillPolygon(brushs, roofPoints);
+
+            // Создаем объект brushes, который используется для заполнения двери, задаем цвет красный
+            Brush brushes = new SolidBrush(Color.Red);
+            // Заполняем дверь
+            g.FillRectangle(brushes, 180, 200, 40, 50);
+
+            // Создаем объект brushed, который используется для заполнения окон, задаем цвет синий
+            Brush brushed = new SolidBrush(Color.Blue);
+            // Заполняем окно 1
+            g.FillRectangle(brushed, 120, 130, 40, 40);
+            g.DrawLine(pen, 140, 130, 140, 170);
+            g.DrawLine(pen, 120, 150, 160, 150);
+            g.FillRectangle(brushed, 240, 130, 40, 40);
+            // Заполняем окно 2
+            g.DrawLine(pen, 260, 130, 260, 170);
+            g.DrawLine(pen, 240, 150, 280, 150);
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
